27f1c6d71b3221a340cbd00fd75cfd8c3fb0acc9
askeladdk <askeladdk@users.noreply.github.com>
2022-05-20 23:17:09 +0200

    Add token unit tests

--- token.go
+++ token.go
@@ -4,31 +4,36 @@ import "fmt"
 
 // Position represents the position of a Token in the input string.
 type Position struct {
-	// Filename is the filename of the input.
+	// Filename is the filename of the input, if any.
 	Filename string
 
-	// Offset is the byte offset in the input.
+	// Offset is the byte offset, starting at 0.
 	Offset int
 
-	// Line is the line number.
+	// Line is the line number, starting at 1.
 	Line int
 
-	// Column is the character from the line.
+	// Column is the column number, starting at 1 (character count per line).
 	Column int
 }
 
-// IsValid reports whether the position is valid.
+// IsValid reports whether a Position is valid.
+// A Position is valid if its Line field is greater than zero.
 func (p Position) IsValid() bool {
-	return p.Line > 0 && p.Column > 0
+	return p.Line > 0
 }
 
+// String implements fmt.Stringer.
 func (p Position) String() string {
 	filename := p.Filename
 	if filename == "" {
 		filename = "<input>"
 	}
 	if p.IsValid() {
-		return fmt.Sprintf("%s:%d:%d", filename, p.Line, p.Column)
+		if p.Column > 0 {
+			return fmt.Sprintf("%s:%d,%d", filename, p.Line, p.Column)
+		}
+		return fmt.Sprintf("%s:%d", filename, p.Line)
 	}
 	return filename
 }
@@ -44,6 +49,7 @@ type Token struct {
 	Text string
 }
 
+// String implements fmt.Stringer.
 func (t Token) String() string {
-	return fmt.Sprintf("%s: (%d) '%s'", t.Position, t.Kind, t.Text)
+	return fmt.Sprintf("%s: '%s'", t.Position, t.Text)
 }
---
+++ token_test.go
@@ -0,0 +1,55 @@
+package prattle
+
+import "testing"
+
+func TestPositionString(t *testing.T) {
+	for _, testCase := range []struct {
+		Name   string
+		Pos    Position
+		Expect string
+	}{
+		{
+			Name:   "ZeroValue",
+			Pos:    Position{},
+			Expect: "<input>",
+		},
+		{
+			Name: "LineOnly",
+			Pos: Position{
+				Filename: "hello.txt",
+				Line:     13,
+			},
+			Expect: "hello.txt:13",
+		},
+		{
+			Name: "LineAndColumn",
+			Pos: Position{
+				Filename: "hello.txt",
+				Line:     13,
+				Column:   37,
+			},
+			Expect: "hello.txt:13,37",
+		},
+	} {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if testCase.Pos.String() != testCase.Expect {
+				t.Error()
+			}
+		})
+	}
+}
+
+func TestTokenString(t *testing.T) {
+	tok := Token{
+		Position: Position{
+			Filename: "hello.txt",
+			Line:     13,
+			Column:   37,
+		},
+		Text: "123",
+	}
+
+	if tok.String() != "hello.txt:13,37: '123'" {
+		t.Error()
+	}
+}
